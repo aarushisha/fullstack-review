341fddcdbceb2825913b04b5d2925c2a357bdda1
aarushis.s.90@gmail.com <aarushis.s.s90@gmail.com>
2019-03-08 13:02:51 -0800

    heroku

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/fetcher');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   ownerLogin: String,
